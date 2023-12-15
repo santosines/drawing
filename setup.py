c8db3828ad9c08297e158065add5ce2dfb5509be
santosines <ines.matos.santos@ubi.pt>
2023-12-15 12:29:48 +0000

    1st

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name="drawing",
+    version="0.1",
+    description="Drawing Distribution Package",
+    packages=find_packages(),
+    include_package_data=True
+)
